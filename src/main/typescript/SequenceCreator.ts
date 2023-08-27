8dbb456d89d940c7e0cdc9dcca4d76ac1fd7605b
Francesco Corso <fcorso2016@gmail.com>
2023-08-26 22:28:07 -0400

    Started work on a set up to create the sequences

---
+++ src/main/typescript/SequenceCreator.ts
@@ -0,0 +1,20 @@
+class SequenceCreator {
+    source : SourceClips;
+    templateName : string;
+
+    constructor(src : SourceClips, template : string) {
+        this.source = src;
+        this.templateName = template;
+    }
+
+    createProject(dest : ProjectItem) : void {
+        var folder = dest.createBin(this.templateName);
+        
+        var sourceClips = this.source.getSourceClips();
+        for (var i : number = 0; i < sourceClips.length; i++) {
+            var clip = sourceClips[i].getClip();
+            var newSequence = app.project.createNewSequenceFromClips(clip.name, [clip], folder);
+            newSequence.videoTracks[0].clips[0].
+        }
+    }
+}
---
+++ src/main/typescript/SourceClip.ts
@@ -0,0 +1,15 @@
+class SourceClip {
+    private clip : ProjectItem;
+
+    constructor(file : ProjectItem) {
+        if (file.type != ProjectItemType.FILE && file.type != ProjectItemType.CLIP) {
+            throw new Error("Cannot create a source clip from something that is not a file")
+        }
+
+        this.clip = file;
+    }
+
+    public getClip() : ProjectItem {
+        return this.clip;
+    }
+}
