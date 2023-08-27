51b660f24ba17f9278906bba16840de89f9a1970
Francesco Corso <fcorso2016@gmail.com>
2023-08-27 09:25:17 -0400

    Moved the typescript files around

--- src/main/typescript/SequenceCreator/SequenceCreator.ts
+++ src/main/typescript/SequenceCreator/SequenceCreator.ts
@@ -14,7 +14,6 @@ class SequenceCreator {
         for (var i : number = 0; i < sourceClips.length; i++) {
             var clip = sourceClips[i].getClip();
             var newSequence = app.project.createNewSequenceFromClips(clip.name, [clip], folder);
-            newSequence.videoTracks[0].clips[0].
         }
     }
 }
